99896114841887f8864596e96ae9991175ff6bf8
Eduardo Almeida <me@edr.io>
2020-04-16 18:42:42 +0100

    Initial Commit

---
+++ FruityKit/FruityKit.h
@@ -0,0 +1,19 @@
+//
+//  FruityKit.h
+//  FruityKit
+//
+//  Created by Eduardo Almeida on 16/04/2020.
+//  Copyright © 2020 Eduardo Almeida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FruityKit.
+FOUNDATION_EXPORT double FruityKitVersionNumber;
+
+//! Project version string for FruityKit.
+FOUNDATION_EXPORT const unsigned char FruityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruityKit/PublicHeader.h>
+
+
